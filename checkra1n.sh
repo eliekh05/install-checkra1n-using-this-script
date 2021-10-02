ec837e9f5cf2e68e55bb2aeddc8d8e86b93e9413
Elie Khalil <eliekh05@gmail.com>
2021-10-02 18:10:54 +0300

    Update checkra1n.sh

--- checkra1n.sh
+++ checkra1n.sh
@@ -2,5 +2,6 @@ echo you need to have apt package manager to do this if you don't have it you ca
 
 echo 'deb https://assets.checkra.in/debian /' | sudo tee /etc/apt/sources.list.d/checkra1n.list
 sudo apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
-sudo apt-get update
-sudo apt-get install checkra1n
+sudo apt-get -y update
+and DON'T FORGET TO DO sudo apt-get -y upgrade to get latest packages 
+sudo apt-get install -y checkra1n
